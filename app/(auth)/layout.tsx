7587afc7d8ce9f11b6088f550f6eddf1b8374744
readul-islam <developer.readul@gmail.com>
2023-09-21 00:54:13 +0600

    layout define

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,21 @@
+import { ClerkProvider } from "@clerk/nextjs";
+import { Inter } from "next/font/google";
+import '../globals';
+export const metadata = {
+  title: "Threads",
+  description: "A Next.js 13 Meta Threads Application",
+};
+
+const inter = Inter({ subsets: ["latin"] });
+
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${inter.className} bg-dark-2`}>{children}</body>
+      </html>
+    </ClerkProvider>
+  );
+};
+
+export default RootLayout;
